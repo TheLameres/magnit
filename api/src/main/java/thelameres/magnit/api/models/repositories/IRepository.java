23f050aefd05ffa038fafdccc01ea03548cdf7fc
TheLameres <xthe.lameresx@gmail.com>
2021-09-23 08:33:25 +0300

    Revert "Init commit"
    This reverts commit 0d149934aa72f2416a4092db7e07775b1fa5b971.

--- api/src/main/java/thelameres/magnit/api/models/repositories/IRepository.java
+++
@@ -1,4 +0,0 @@
-package thelameres.magnit.api.models.repositories;
-
-public interface IRepository<T> {
-}
--- api/src/main/java/thelameres/magnit/api/xml/XmlParser.java
+++
@@ -1,8 +0,0 @@
-package thelameres.magnit.api.xml;
-
-import javax.xml.bind.JAXBException;
-import java.math.BigDecimal;
-
-public interface XmlParser {
-    BigDecimal sum(String fileName) throws JAXBException;
-}
